a4e422eb81bbfcc3ad97f867441bfb5f39720ee5
Smitthyy <96345069+Smitthyy@users.noreply.github.com>
2022-10-27 00:25:39 +0100

    Added vanrobbery export

--- client/cl_events.lua
+++ client/cl_events.lua
@@ -614,6 +614,32 @@ end
 
 exports('Explosion', Explosion)
 
+local function VanRobbery()
+    local currentPos = GetEntityCoords(PlayerPedId())
+    local locationInfo = getStreetandZone(currentPos)
+    local gender = GetPedGender()
+    TriggerServerEvent("dispatch:server:notify", {
+        dispatchcodename = "vanrobbery", -- has to match the codes in sv_dispatchcodes.lua so that it generates the right blip
+        dispatchCode = "10-90",
+        firstStreet = locationInfo,
+        gender = gender,
+        model = nil,
+        plate = nil,
+        priority = 2, -- priority
+        firstColor = nil,
+        automaticGunfire = false,
+        origin = {
+            x = currentPos.x,
+            y = currentPos.y,
+            z = currentPos.z
+        },
+        dispatchMessage = _U('vanrobbery'), -- message
+        job = { "police" } -- jobs that will get the alerts
+    })
+end
+
+exports('VanRobbery', VanRobbery)
+
 local function SuspiciousActivity()
     local currentPos = GetEntityCoords(PlayerPedId())
     local locationInfo = getStreetandZone(currentPos)
